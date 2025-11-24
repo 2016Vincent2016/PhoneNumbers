a6536292b65abb72856a9eb7dce00cc7bb1c51df
Vincent W. <pc2025weingaertner@gmail.com>
2025-11-24 16:37:57 +0100

    created first commit

---
+++ main.py
@@ -0,0 +1,13 @@
+names = ['Квам-Дамн', 'Лук Скамворкер', 'Петард Вейпер', 'Лия Моргала', 'Эдуард Скамворкер']
+phones = ['-79899899889', '112', '1', '+09998765432', '0']
+
+book_phones = {
+    names[0]: phones[0],
+    names[1]: phones[1],
+    names[2]: phones[2],
+    names[3]: phones[3],
+    names[4]: phones[4],
+
+}
+
+print(book_phones)
